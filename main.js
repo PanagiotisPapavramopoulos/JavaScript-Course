5d577f88a2f6c8a6d048cbb3ac16fd15ecc4b8ad
Panagiotis Papavramopoulos <viper.dnd@gmail.com>
2020-03-29 08:37:05 +0300

    updates

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
+/* 
+1st Example
+
+document.getElementById("button").onclick = function() {
+  document.getElementById("confirm").innerHTML = "Purchace Confirmed. Check your Email.";
+  document.getElementById("button").style.display = "none";
+}
+*/
+
+
+/* 
+2nd Example
+
+var username = prompt();
+console.log(username);
+ */
+
+/* 
+3rd Example
+
+ var age; //Declation
+ age = 50; //initialization
+ */
+
+/* 
+4th Example
+
+(function(){
+      //IFEE - immediately invoked function expression
+  var age =50;
+})(); 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
